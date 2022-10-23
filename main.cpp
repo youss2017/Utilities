77d5d97beb8293108731886c7aad337b73d63e0b
Your Name <you@example.com>
2022-10-23 12:53:15 -0400

    Fix bugs & Add Network Interface Query

--- main.cpp
+++ main.cpp
@@ -6,13 +6,21 @@
 
 int main() {
 	sw::Startup();
+
+	auto adapters = sw::Socket::EnumerateNetworkAdapters();
+	for (auto& a : adapters) {
+		LOG(INFO, "\n{0}", std::string(a));
+	}
+	return 0;
+
 	sw::Socket s(sw::SocketType::UDP);
 	s.Bind(sw::SocketInterface::Any, 4000).EnableBroadcast();
 
 	while (true) {
-		s.SendTo("hello", 5, "192.168.1.255", 3000);
-		int x = WSAGetLastError();
+		auto b = s.SendTo("hello", 5, "255.255.255.255", 3000);
+		int e = WSAGetLastError();
 		std::this_thread::sleep_for(std::chrono::milliseconds(2));
+		LOG(INFO, "{0} Send Bytes, Error {1}", b, e);
 	}
 
 
